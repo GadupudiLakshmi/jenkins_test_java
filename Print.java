07203c864b493d6ea038e6ccff1096029c81e1b9
GadupudiLakshmi <87761793+GadupudiLakshmi@users.noreply.github.com>
2021-08-03 10:28:09 +0530

    Update Print.java

--- Print.java
+++ Print.java
@@ -1,7 +1,7 @@
 class Print 
 {
 	public void print (int value) {
-		System.out.println ("This is print function\n");
+		System.out.println ("This is print class\n");
 		System.out.println (value);
 	}
 }
