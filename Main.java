22337a445ebd1d2b005d80ad6b6bb2e4481c4f47
Lakshmi Gadupudi <lakshmigadupud@gmail.com>
2021-08-03 20:04:34 +0530

    wrong

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main
 	public static void main (String ar[]) {
 		int result;
 		
-		Calculator calc = new Calculator ();
+		Calculator calc = new Calculato ();
 		result = calc. Add (1,2);
 
 		Print data = new Print ();
