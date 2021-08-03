211094fa710a21d7e3ad529533cd4e8210299f17
GadupudiLakshmi <87761793+GadupudiLakshmi@users.noreply.github.com>
2021-08-03 11:16:05 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 class Calculator
 {
 	public int Add (int num1, int num2) {
-		System.out.println ("This is add function \n");
+		System.out.println ("This is add function in calc class\n");
 		return num1 + num2;
 	}
 }
